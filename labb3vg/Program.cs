e82c852b80f8a74b6fa88de6719ad6580b03f5b1
Svante Joelsson <svante.joelsson@gmail.com>
2021-01-11 08:25:06 +0100

    Add project files.

---
+++ labb3vg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace labb3vg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
